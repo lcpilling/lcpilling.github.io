ba3d5348c52e0f3a55ad9f342f8b2988285c48f9
Luke C. Pilling <luke.pilling@gmail.com>
2023-09-19 14:50:49 +0100

    Add twitter plot

---
+++ files/jane_twitter.R
@@ -0,0 +1,22 @@
+library(ThemePark)
+library(tidyverse)
+
+d = read_csv("https://raw.githubusercontent.com/lukepilling/lukepilling.github.io/files/jane_twitter.csv")
+
+cor(d$x, d$y)^2
+
+ggplot(d, aes(x = x, y = y)) +
+  geom_smooth(method = 'lm', 
+              color  = barbie_theme_colors["dark"],
+              fill   = barbie_theme_colors["light"]) +
+  geom_point(color = barbie_theme_colors["medium"]) +
+  labs(title = "",
+       caption  = "R^2 = 0.0486",
+       x = "x",
+       y = "y") +
+  xlim(c(0,1)) +
+  ylim(c(0,1)) +
+  theme_barbie()
+
+
+
